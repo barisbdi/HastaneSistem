5116ae145496025bcec003b163bad417a5b3dde0
barisbdi <barisbdci@gmail.com>
2024-09-16 18:56:21 +0300

    Test2

--- HastaneSistem/Form1.Designer.cs
+++ HastaneSistem/Form1.Designer.cs
@@ -44,7 +44,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(93, 68);
+            this.button2.Location = new System.Drawing.Point(122, 68);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
